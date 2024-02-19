84d9f45394305a3a29784268ab934b30b1b1fc8f
Александр Рубанов <uttka90@gmail.com>
2024-02-19 12:33:47 +0300

    Update assert_color.py

--- Pages/assert_color.py
+++ Pages/assert_color.py
@@ -1,12 +1,1 @@
-from selenium.webdriver.common.by import By
 
-class assert_color:
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-
-    def color(self, name):
-       green_rgba = "rgba(209, 231, 221, 1)"
-       name =  self.driver.find_element(By.CSS_SELECTOR, "#first-name").value_of_css_property("background-color")
-       assert name == green_rgba
